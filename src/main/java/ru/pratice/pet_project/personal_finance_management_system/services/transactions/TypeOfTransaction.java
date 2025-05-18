c01e2cc8b8131c2ed5e0b1b0b477671933a7caa1
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2025-05-18 17:01:07 +0300

    fix bug with name()

--- src/main/java/ru/pratice/pet_project/personal_finance_management_system/services/transactions/TypeOfTransaction.java
+++ src/main/java/ru/pratice/pet_project/personal_finance_management_system/services/transactions/TypeOfTransaction.java
@@ -2,10 +2,8 @@ package ru.pratice.pet_project.personal_finance_management_system.services.trans
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.experimental.Accessors;
 
 @Getter
-@Accessors(fluent = true)
 @AllArgsConstructor
 public enum TypeOfTransaction {
     INCOME_TYPE("доход"),
